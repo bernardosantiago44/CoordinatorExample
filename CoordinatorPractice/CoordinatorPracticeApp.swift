ed1221f45bfba43c55692117c620eabb5b1c1b1e
Bernardo Santiago <bernardosantiago44@gmail.com>
2025-05-14 18:14:57 -0600

    Initial Commit

---
+++ CoordinatorPractice/CoordinatorPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatorPracticeApp.swift
+//  CoordinatorPractice
+//
+//  Created by Bernardo Santiago Marin on 14/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CoordinatorPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
